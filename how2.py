6cc819e2ac92485fa64b748780530e71974a0590
howbin <howbin13191201638@outlook.com>
2020-01-16 01:24:05 +0800

    how2 capter_4 开始

---
+++ how2.py
@@ -0,0 +1,14 @@
+""" 第 4 章 操作列表 """
+""" 遍历整个列表 """
+magicians = ['alice', 'david', 'carolina']
+i = 1
+for magician in magicians:
+    print(str(i)+"\n"+magician)
+    print('wawaw')
+    i = i+1
+print('game over')
+""" 创建数值列表 """
+for values in range(1, 5):
+    print(values)
+numbers = list(range(1, 6))
+print(numbers)
